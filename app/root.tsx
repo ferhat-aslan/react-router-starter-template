7a9955e0f7beeeca8dec68353914b852384dd8fe
Ferhat Aslan <aslanferhat16@gmail.com>
2026-08-21 04:48:19 +0200

    favicon.ico changed

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import appcss from "./app.css?inline";
 import {getTranslationData} from "./utils/route-utils";
 
 export const links: Route.LinksFunction = () => [
+  {rel: "icon", href: "/favicon.ico?v=2", type: "image/x-icon"},
   /* {rel: "preconnect", href: "https://fonts.googleapis.com"},
   {
     rel: "preconnect",
